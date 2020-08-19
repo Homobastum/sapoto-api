76733ce1be501326a9a846224011d6596e387c71
Mathieu GONTHIER <gonthier.mhc@gmail.com>
2020-08-19 13:29:26 +0400

    Faille de sécurité: cloisonner utilisateur pour MongoDB

--- backend/src/services/support/SupportService.ts
+++ backend/src/services/support/SupportService.ts
@@ -5,7 +5,7 @@ import ticketRouting from "./routes/TicketRouting";
 export class SupportService implements Service {
     bdd: MongoDB;
     uri: string =
-        "mongodb+srv://Homobastum:sloiieOs6O4a3ds0@hb-lab.iajpl.mongodb.net/sapoto?retryWrites=true&w=majority";
+        "mongodb+srv://github:27rZKXFnDsea3EtV@hb-lab.iajpl.mongodb.net/sapoto?retryWrites=true&w=majority";
 
     constructor(public app: any) {
         const bdd = new MongoDB(this.uri);
--- backend/src/services/user/UserService.ts
+++ backend/src/services/user/UserService.ts
@@ -5,7 +5,7 @@ import userRouting from "./routes/UserRouting";
 export class UserService implements Service {
     bdd: MongoDB;
     uri: string =
-        "mongodb+srv://Homobastum:sloiieOs6O4a3ds0@hb-lab.iajpl.mongodb.net/sapoto?retryWrites=true&w=majority";
+        "mongodb+srv://github:27rZKXFnDsea3EtV@hb-lab.iajpl.mongodb.net/sapoto?retryWrites=true&w=majority";
 
     constructor(public app: any) {
         const bdd = new MongoDB(this.uri);
